b16211f907a30b31fc13672e3f14038cf3f1d8c5
Grant Jenks <grant.jenks@gmail.com>
2022-02-26 00:21:22 -0800

    Add pillow to dependencies

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setuptools.setup(
         'appdirs',
         'django==3.2.*',
         'gunicorn',
+        'pillow',
         'rumps @ git+https://github.com/jaredks/rumps.git@018cabce9175aed3a77b747dc50118cd65a5374d',
     ],
 )
