f59caa96c36a76ef6cd844bdd9b4fad084b0dd67
Grant Jenks <grant.jenks@gmail.com>
2022-02-26 00:22:07 -0800

    Setup media

--- emma/settings.py
+++ emma/settings.py
@@ -130,7 +130,15 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Media
+
+MEDIA_DIR = DATA_DIR / 'media'
+MEDIA_DIR.mkdir(parents=True, exist_ok=True)
+MEDIA_URL = '/media/'
