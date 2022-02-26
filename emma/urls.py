148f8dd032984b5ca21b38051aa07be1e3b0d064
Grant Jenks <grant.jenks@gmail.com>
2022-02-26 00:54:12 -0800

    Serve media urls

--- emma/urls.py
+++ emma/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.shortcuts import redirect
 from django.urls import path
@@ -24,4 +26,5 @@ admin.site.site_title = 'Emma'
 urlpatterns = [
     path('', lambda request: redirect('/emma/')),
     path('emma/', admin.site.urls),
+    *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 ]
