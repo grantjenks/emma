b62e75e23524892a604f19e35c030793c321a25a
Grant Jenks <grant.jenks@gmail.com>
2022-02-25 20:52:45 -0800

    Update rumps to support Python 3.10

--- setup.py
+++ setup.py
@@ -14,6 +14,6 @@ setuptools.setup(
         'appdirs',
         'django==3.2.*',
         'gunicorn',
-        'rumps',
+        'rumps @ git+https://github.com/jaredks/rumps.git@018cabce9175aed3a77b747dc50118cd65a5374d',
     ],
 )
