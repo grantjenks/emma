54367f90bab44494feda1ddfc540a61c90018a71
Grant Jenks <grant.jenks@gmail.com>
2022-02-26 00:22:44 -0800

    Add screenshot model

--- emma/models.py
+++ emma/models.py
@@ -1,1 +1,7 @@
 from django.db import models
+
+
+class Screenshot(models.Model):
+    num = models.IntegerField()
+    time = models.DateTimeField()
+    image = models.ImageField(upload_to='screenshots/%Y/%m/%d/%H/%M/')
