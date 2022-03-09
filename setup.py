933ad550790961fcf84c42c4280aaa66fb755387
Grant Jenks <grant.jenks@gmail.com>
2022-03-08 20:14:46 -0800

    Add pandas and plotly to dependencies

--- setup.py
+++ setup.py
@@ -25,7 +25,9 @@ setuptools.setup(
         'appdirs',
         'django==3.2.*',
         'gunicorn',
+        'pandas',
         'pillow',
+        'plotly',
         'rumps @ git+https://github.com/jaredks/rumps.git@018cabce9175aed3a77b747dc50118cd65a5374d',
     ],
     entry_points = {
