c476202046a2e4ce248856bbfc4b89bbc799d205
Grant Jenks <grant.jenks@gmail.com>
2022-04-05 22:53:42 -0700

    Fix "rumps" dependency for twine upload

--- setup.py
+++ setup.py
@@ -28,7 +28,8 @@ setuptools.setup(
         'pandas',
         'pillow',
         'plotly',
-        'rumps @ git+https://github.com/jaredks/rumps.git@018cabce9175aed3a77b747dc50118cd65a5374d',
+        'rumps',
+        # 'rumps @ git+https://github.com/jaredks/rumps.git@018cabce9175aed3a77b747dc50118cd65a5374d',
     ],
     entry_points = {
         'console_scripts': ['emma=emma.__main__:main'],
